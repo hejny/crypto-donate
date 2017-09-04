fe0e7ebce5cb1c686aa61453413fcf6f9b43c0e7
hejny <me@pavolhejny.com>
2017-09-04 11:45:50 +0200

    - cleaning

---
+++ src/components/donate.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+
+export function Component(props) {
+
+    let {xxx, yyy} = props;
+
+    return (
+        <div>
+            hello
+        </div>
+    );
+
+}
